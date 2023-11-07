19451c808b8cd7f8e1dbd2dd5dbd752742109a0a
lakient <modenoff.nik@gmail.com>
2023-11-07 16:51:44 +0500

    add some base models

--- api/pyd/base_models.py
+++ api/pyd/base_models.py
@@ -3,16 +3,45 @@ from pydantic import BaseModel, Field
 
 # файл с базовыми моделями
 
-class LessonBase(BaseModel):
-    title: str = Field(..., example='Название/Заголовок/Title')
-    text: str = Field(..., example='Text')
+
+class LevelBase(BaseModel):
+    id: int = Field(..., gt=0, example=1)
+    level_name: str = Field(None)
+
+class SubjectBase(BaseModel):
+    id: int = Field(..., gt=0, example=1)
+    subject_name: str = Field(None)
+
+
+class AnswerBase(BaseModel):
+    id: int = Field(..., gt=0, example=1)
+    answer_text: str = Field(None)
+
+class QuestionBase(BaseModel):
+    id: int = Field(..., gt=0, example=1)
+    answer: AnswerBase
+
+class TestBase(BaseModel):
+    id: int = Field(..., gt=0, example=1)
+    title: str = Field(None, example='Название/Заголовок/Title')
+    isFinished: bool = Field(False)
+    favorite: bool = Field(False)
+    questions: QuestionBase
+    level: LevelBase
+    subject: SubjectBase
 
     class Config:
         orm_mode = True
 
-class TestBase(BaseModel):
-    title: str = Field(..., example='Название/Заголовок/Title')
-    text: str = Field(..., example='Text')
+class LessonBase(BaseModel):
+    id: int = Field(..., gt=0, example=1)
+    title: str = Field(None, example='Название/Заголовок/Title')
+    text: str = Field(None, example='Text')
+    isFinished: bool = Field(False)
+    favorite: bool = Field(False)
+    test: TestBase
+    subject: SubjectBase
+    level: LevelBase
 
     class Config:
         orm_mode = True
