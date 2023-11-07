82342193c72659ccd871ae10652caa2d24a27fdb
lakient <modenoff.nik@gmail.com>
2023-11-07 17:18:47 +0500

    fix bug

--- api/models.py
+++ api/models.py
@@ -2,33 +2,33 @@ from sqlalchemy import Numeric, Table, Boolean, Column, ForeignKey, Integer, Str
 from sqlalchemy.orm import relationship
 from database import Base
 
-# Вспомогательная таблица для связи многие ко многим у категорий и продуктов
-product_category = Table('product_category', Base.metadata,
-                         Column('product_id', ForeignKey('products.id'), primary_key=True),
-                         Column('category_id', ForeignKey('categories.id'), primary_key=True)
-                         )
+# # Вспомогательная таблица для связи многие ко многим у категорий и продуктов
+# product_category = Table('product_category', Base.metadata,
+#                          Column('product_id', ForeignKey('products.id'), primary_key=True),
+#                          Column('category_id', ForeignKey('categories.id'), primary_key=True)
+#                          )
 
 
-class Category(Base):
-    __tablename__ = "categories"
-
-    id = Column(Integer, primary_key=True)
-    name = Column(String(255), unique=True)
-    description = Column(String(255), nullable=True)
-    # Если использовать backref то необезательно указывать связи в обеих таблицах
-    # products = relationship('Product', secondary="product_category", back_populates='categories')
-
-
-class Product(Base):
-    __tablename__ = "products"
-
-    id = Column(Integer, primary_key=True)
-    name = Column(String(255))
-    description = Column(String(255), nullable=True)
-    price = Column(Numeric, default=0)
-
-    # backref автоматически делает связь в другой таблице
-    categories = relationship("Category", secondary="product_category", backref="products")
+# class Category(Base):
+#     __tablename__ = "categories"
+#
+#     id = Column(Integer, primary_key=True)
+#     name = Column(String(255), unique=True)
+#     description = Column(String(255), nullable=True)
+#     # Если использовать backref то необезательно указывать связи в обеих таблицах
+#     # products = relationship('Product', secondary="product_category", back_populates='categories')
+#
+#
+# class Product(Base):
+#     __tablename__ = "products"
+#
+#     id = Column(Integer, primary_key=True)
+#     name = Column(String(255))
+#     description = Column(String(255), nullable=True)
+#     price = Column(Numeric, default=0)
+#
+#     # backref автоматически делает связь в другой таблице
+#     categories = relationship("Category", secondary="product_category", backref="products")
 
 
 class User(Base):
@@ -48,13 +48,8 @@ class Lesson(Base):
     favorite = Column(Boolean, default=False)
 
     subject = relationship("Subject", backref="subjects")
-    subject_id = Column(Integer, ForeignKey( "subjects.id"))
-
     level = relationship("Level", backref="levels")
-    level_id = Column(Integer, ForeignKey("levels.id"))
-
     test = relationship("Test", backref="tests")
-    test_id = Column(Integer, ForeignKey("tests.id"))
 
     # need to connect:
     # subject+, level+, tests+
@@ -67,14 +62,9 @@ class Test(Base):
     isFinished = Column(Boolean, default=False)
     favorite = Column(Boolean, default=False)
 
-    subject = relationship("Test", backref="tests")
-    subject_id = Column(Integer, ForeignKey("subjects.id"))
-
+    subject = relationship("Subject", backref="subjects")
     level = relationship("Level", backref="levels")
-    level_id = Column(Integer, ForeignKey("levels.id"))
-
     questions = relationship("Question", backref="questions")
-    questions_id = Column(Integer, ForeignKey("questions.id"))
 
     # need to connect
     # subject+, questions+
@@ -86,7 +76,6 @@ class Question(Base):
     question_text = Column(String(100))
 
     answer = relationship("Answer", backref="answers")
-    answer_id = Column(Integer, ForeignKey("answers.id"))
 
     # need ro connect
     # answer+
